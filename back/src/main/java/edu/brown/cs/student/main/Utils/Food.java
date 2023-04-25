12479d87845d69322cbb13a8cd47ae490431c931
yonasA2025 <yonas_amha@brown.edu>
2023-04-25 13:11:13 -0400

    food class added

---
+++ back/src/main/java/edu/brown/cs/student/main/Utils/Food.java
@@ -0,0 +1,39 @@
+package Utils;
+
+public class Food {
+
+    private String food, description;
+    private boolean isVegan, isVegetarian, isHalal,isKosher, isLactoseFree;
+    private double rating;
+
+
+    public Food(String food, String description, double rating) {
+        this.food = food;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public Food(String food, double rating){
+        this.food = food;
+        this.rating = rating;
+    }
+
+    public Food(String food, String description, boolean isVegan, boolean isVegetarian, boolean isHalal, boolean
+            isKosher, boolean isLactoseFree, double rating){
+
+        this.food = food;
+        this.description = description;
+        this.isVegan = isVegan;
+        this.isVegetarian = isVegetarian;
+        this.isHalal = isHalal;
+        this.isKosher = isKosher;
+        this.isLactoseFree = isLactoseFree;
+        this.rating = rating;
+    }
+
+
+    public String toString(){
+        return "Food: "+ food + "\n Description: " + description;
+    }
+
+}
